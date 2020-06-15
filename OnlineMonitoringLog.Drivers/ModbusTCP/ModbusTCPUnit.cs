db8bfafb67a934284c5b3fffe66766fada3e32ae
mrhb <mohammad_hajjar@yahoo.com>
2020-06-15 15:33:18 +0430

    add Modbus TCP

--- OnlineMonitoringLog.Drivers/ModbusTCP/ModbusTCPUnit.cs
+++ OnlineMonitoringLog.Drivers/ModbusTCP/ModbusTCPUnit.cs
@@ -13,6 +13,7 @@ using OnlineMonitoringLog.Core;
 using OnlineMonitoringLog.Core.Interfaces;
 using System.ComponentModel;
 using InfluxDB.Collector;
+using EasyModbus;
 
 namespace OnlineMonitoringLog.Drivers.IEC104
 {
@@ -43,49 +44,46 @@ namespace OnlineMonitoringLog.Drivers.IEC104
 
         private void ProcessUa()
         {
-            Console.WriteLine("Connecting....in theread" + LibraryCommon.GetLibraryVersionString());
-            Connection con = new Connection("127.0.0.1", 2404);//Ip.ToString());
-
-            con.DebugOutput = false;
-
-            con.SetASDUReceivedHandler(asduReceivedHandler, null);
-            con.SetConnectionHandler(ConnectionHandler, null);
-
-            try
+            while (true)
             {
-                con.Connect();
-                ConnectionTimer = null;
+                try
+                {
+                    ModbusClient modbusClient = new ModbusClient("127.0.0.1", 502);    //Ip-Address and Port of Modbus-TCP-Server
+                    modbusClient.Connect();                                                    //Connect to Server
+                    modbusClient.WriteMultipleCoils(4, new bool[] { true, true, true, true, true, true, true, true, true, true });    //Write Coils starting with Address 5
+                                                                                                                                      //bool[] readCoils = modbusClient.ReadCoils(9, 10);                        //Read 10 Coils from Server, starting with address 10
+                    while (true)
+                    {
+                        int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(0, 12);    //Read 10 Holding Registers from Server, starting with Address 1
+                        var datas = new Dictionary<string, object>();
+                        // Console Output
+
+                        for (int i = 1; i < readHoldingRegisters.Length; i++)
+                        {
+                            Console.WriteLine("Value of HoldingRegister " + (i + 1) + " " + readHoldingRegisters[i].ToString());
+                            int val = readHoldingRegisters[i];
+                            var item = Variables.Where(p => ((ModbusTCPVariable)p).ObjectAddress == i).First();
+                            item.RecievedData(val, DateTime.Now);
+
+
+                            Metrics.Increment("mrhb_iterations");
+                            datas.Add(item.name.ToString() + "_" + ID.ToString(), val);
+                        }
+                        Metrics.Write("UIWPF", datas);
+
+                        Thread.Sleep(1000);
+                    }
+                }
+                catch (Exception c)
+                {
+                    Console.Write($"Error Occured in {t.Name}: {c.ToString()}");
+                }
             }
-            catch (Exception C)
-            {
-                Console.WriteLine(C.Message);
-                ConnectionTimer = new Timer(ConnectToIec104Server, null, 0, 5000);
-            }
+           
+           
         }
 
-        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
-            Console.WriteLine("Connecting....in BackgroundWorker" + LibraryCommon.GetLibraryVersionString());
-            Connection con = new Connection("127.0.0.1", 2404);//Ip.ToString());
-
-            con.DebugOutput = false;
-
-            con.SetASDUReceivedHandler(asduReceivedHandler, null);
-            con.SetConnectionHandler(ConnectionHandler, null);
-
-            try
-            {
-                con.Connect();
-                ConnectionTimer = null;
-            }
-            catch (Exception C)
-            {
-                Console.WriteLine(C.Message);
-                ConnectionTimer = new Timer(ConnectToIec104Server, null, 0, 5000);
-            }
-
-        }
-        private void ConnectToIec104Server(object state)
+       private void ConnectToIec104Server(object state)
 
         {
             Console.WriteLine("Connect to Iec104Server Using lib60870.NET version " + LibraryCommon.GetLibraryVersionString());
@@ -109,7 +107,7 @@ namespace OnlineMonitoringLog.Drivers.IEC104
 
         }  
         
-        public override string ToString() { return "IEC104: " + Ip.ToString(); }
+        public override string ToString() { return "ModbusTCP: " + Ip.ToString(); }
        
 
         private void ConnectionHandler(object parameter, ConnectionEvent connectionEvent)
@@ -141,23 +139,7 @@ namespace OnlineMonitoringLog.Drivers.IEC104
             if (asdu.TypeId == TypeID.M_ME_TF_1)
             {
 
-                for (int i = 0; i < asdu.NumberOfElements; i++)
-                {
-
-                    var val = (MeasuredValueShortWithCP56Time2a)asdu.GetElement(i);
-                    var item = Variables.Where(p => ((IEC104Variable)p).ObjectAddress == val.ObjectAddress).First();
-                    item.RecievedData((int)val.Value, DateTime.Now);
-
-
-                    Metrics.Increment("mrhb_iterations");
-                    var datas = new Dictionary<string, object>
-                    {
-                         {item.name.ToString()+"_"+ID.ToString(), (int)val.Value}
-                    };
-                    Metrics.Write("UIWPF", datas);
-
-
-                }
+                
             }
             else
             {
---
+++ OnlineMonitoringLog.Drivers/UnitFactory.cs
@@ -0,0 +1,36 @@
+﻿
+using OnlineMonitoringLog.Core.DataRepository.Entities;
+using OnlineMonitoringLog.Core.Interfaces;
+using OnlineMonitoringLog.Drivers.IEC104;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OnlineMonitoringLog.Drivers
+{ 
+    public class UnitFactory
+    {
+        public static IUnit getUnit(int UnitId, ProtocolType pt, IPAddress ip)
+        {
+            switch (pt)
+            {
+               
+                //case ProtocolType.CoAp:
+                //    return new coapUnit(UnitId, ip);
+                case ProtocolType.IEC104:
+                    return new IEC104Unit(UnitId, ip);
+                case ProtocolType.ModbusTcp:
+                    return new ModbusTCPUnit(UnitId, ip);
+                default:
+                  throw new ApplicationException(string.Format("Unit of Type '{0}' not implemented to be created", pt.ToString()));
+                  
+            }
+         
+        }
+    }
+
+}
