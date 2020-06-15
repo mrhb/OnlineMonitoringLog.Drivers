d5f2d9be8c421c2fb69b7efaf056f75ae84a6ff7
mrhb <mohammad_hajjar@yahoo.com>
2020-06-15 11:22:17 +0430

    seperate concerns

---
+++ OnlineMonitoringLog.Drivers/Types/ObjAddress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMonitoringLog.Drivers.Types
+{
+    public class ObjAddress
+    {
+        public const int InputWaterTemp = 1;
+        public const int OutputWaterTemp = 2;
+        public const int OilPress = 3;
+        public const int AdvanceSpark = 4;
+        public const int ValvePosition = 5;
+        public const int ValveFlow = 6;
+        public const int ExhaustTemp = 7;
+        public const int ElecPower = 8;
+        public const int ElecEnergy = 9;
+        public const int WorkTime = 10;
+        public const int frequency = 11;
+        public const int PowerFactor = 12;
+
+    }
+}
